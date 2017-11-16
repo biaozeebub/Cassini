e387c19f6af5baad114aa04b4358d68c04ee7d27
Biao <biao.boey@gmail.com>
2017-11-15 17:07:42 -0800

    First commit

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,13 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Biao on 11/15/17.
+//  Copyright © 2017 Biao. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let stanford = URL(string: "http://stanford.edu/about/images/intro_about.jpg")
+}
